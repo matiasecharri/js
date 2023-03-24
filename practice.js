c8a515d3031ad64189fe3fc81dcdad4ce3acbd95
matiasecharri <matiasecharri@hotmail.com>
2023-03-24 17:07:01 -0300

    Up

--- practice.js
+++ practice.js
@@ -1,20 +1,14 @@
 
-//Variables, nos permiten almacenar contenido, podemos utilizar const o let
-//segun lo que necesitemos, la diferencia es que LET es una VARIABLE que puede VARIAR
-// y const en cambio se utilizar para cosas que no vayan a varias.
-
-//Variable LET 
 let colorDePelo = "rubio"
 colorDePelo = "castaño"
 
-//Variable CONST 
 const dniMatias = 42364286 
 
-///////////////////////////DATOS PRIMITIVOS
 
-let gato = "COCHOTE" // String
-let edadDeGato = 5 //Number
-let estaCastrado = true //Boolean
+
+let gato = "COCHOTE"
+let edadDeGato = 5 
+let estaCastrado = true 
 let comioHoy = undefined
 let nulo = null
 
