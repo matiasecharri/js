fda341a66bfa9c638fca0f1baee3220242cb2809
matiasecharri <matiasecharri@hotmail.com>
2023-05-31 11:03:29 -0300

    fibonaccifunction

--- fromscratch.js
+++ fromscratch.js
@@ -1,1 +1,12 @@
-console.log("from scratch")
+//Fibonacci
+
+function fibonacci(number) {
+  const fib = [0, 1];
+
+  for (let x = 2; x <= number; x++) {
+    fib[x] = fib[x - 1] + fib[x - 2];
+  }
+  return fib[number];
+}
+
+console.log(fibonacci(4));
