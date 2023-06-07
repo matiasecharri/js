21324dcd8098af4be3111a6e32cb45e72a7213c1
matiasecharri <matiasecharri@hotmail.com>
2023-06-06 22:07:37 -0300

    object properties manipulation

--- fromscratch.js
+++ fromscratch.js
@@ -263,11 +263,18 @@ const humanCreator = (nameOfTheHuman, age, hairColor, emotionalState) => {
   return humanFetus;
 };
 
-const dibellaWeasly = humanCreator("Dibellita",22,"Light Brown","Sad/Shinji");
+const dibellaWeasly = humanCreator("Dibellita", 22, "Light Brown", "Sad/Shinji");
 const matiasPotter = humanCreator("Matias", 24, "Brown", "Neutral");
 const claudito = humanCreator("Claudio", null, "Black");
 const cochito = humanCreator("Cocho", null, "Black and White", "Lazy");
+
 dibellaWeasly.emotionalState = "Happy";
 dibellaWeasly.hairColor = "Light Brown, with black ends";
 dibellaWeasly.palaState = false;
+dibellaWeasly.highestProperties = true;
+dibellaWeasly.personCode = 43043075;
+dibellaWeasly.nameOfTheHuman = dibellaWeasly.nameOfTheHuman.toLowerCase();
+dibellaWeasly.personCode = dibellaWeasly.personCode.toString()
+
 console.log(dibellaWeasly, matiasPotter, claudito, cochito);
+
